a7c7086bdbec688c8eb9034d39973eacbac0eab0
Michael Shpeh <mykhailoshpeh@gmail.com>
2026-03-01 10:51:02 +0200

    emotion 1 step

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,2 +1,19 @@
 import styled from '@emotion/styled';
 
+// .titleRobotoBold {
+//     composes: robotoBold;
+//     margin - bottom: 24px;
+//     font - size: 48px;
+//     text - align: center;
+//     color: darkred;
+// }
+
+export const TitleRobotoBold = styled.h2`
+    margin-bottom: 24px;
+    font-size: 48px;
+    text-align: center;
+     font-family: "Roboto", cursive;
+    font-weight: 700;
+    font-style: normal;
+    color: darkgreen;
+`
